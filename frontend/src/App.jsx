eb1af68dbb002f434f3d37b541ad5aa3f932c8c3
Varga Mark Bence <varga.mark@bolyai.local>
2025-12-05 15:22:21 +0100

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
 import LandingPage from "./pages/LandingPage"
 import OurStoryPage from "./pages/OurStoryPage"
 
 function App() {
   return (
     <div className="app-root">
+      <Navbar />
       <main className="page-main">
         <LandingPage />
       </main>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { NavLink, Link } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <header className="topbar">
+            <div className="topbar-inner">
+                <div className="topbar-left">
+                    <Link to="/" className="brand">
+                        GYMORA
+                    </Link>
+                </div>
+
+                <nav className="topbar-nav">
+                    <a href="#features" className="topbar-link">
+                        Features
+                    </a>
+                    <a href="#pricing" className="topbar-link">
+                        Pricing
+                    </a>
+                    <a href="#about" className="topbar-link">
+                        About
+                    </a>
+                    <NavLink to="/our-story" className="topbar-link"></NavLink>
+                </nav>
+
+                <div className="topbar-right">
+                    <Link to="/login" className="topbar-small-link">
+                        Login
+                    </Link>
+                    <Link to="/register" className="btn btn-nav">
+                        Register
+                    </Link>
+                </div>
+            </div>
+        </header>
+    )
+}
